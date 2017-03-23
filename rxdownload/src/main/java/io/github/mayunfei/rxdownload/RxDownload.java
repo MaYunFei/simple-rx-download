56c5847277e40b766d5fe11dc6cbf4cfa142f19a
MaYunFei <mayunfei6@gmail.com>
2017-03-23 18:40:13 +0800

    rxdownload

--- rxdownload/src/main/java/io/github/mayunfei/rxdownload/RxDownload.java
+++ rxdownload/src/main/java/io/github/mayunfei/rxdownload/RxDownload.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.IBinder;
 import io.github.mayunfei.rxdownload.function.DownloadService;
+import io.github.mayunfei.rxdownload.function.ServiceHelper;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
@@ -25,23 +26,16 @@ import static io.github.mayunfei.rxdownload.function.Utils.log;
 
 public class RxDownload {
 
-
-  private static final Object object = new Object();
   @SuppressLint("StaticFieldLeak") private volatile static RxDownload instance;
 
-  private volatile static boolean bound = false;
-  private int maxDownloadNumber = 5;
-
   private final Context context;
-
-  //信号量  用于绑定service
-  private Semaphore semaphore;
+  private ServiceHelper serviceHelper;
 
   private DownloadService downloadService;
 
   private RxDownload(Context context) {
     this.context = context.getApplicationContext();
-    semaphore = new Semaphore(1);
+    serviceHelper = new ServiceHelper(context);
   }
 
   static {
@@ -56,73 +50,5 @@ public class RxDownload {
         }
       }
     });
-  }
-
-  private Observable<?> createGeneralObservable(final GeneralObservableCallback callback) {
-    return Observable.create(new ObservableOnSubscribe<Object>() {
-      @Override public void subscribe(final ObservableEmitter<Object> emitter) throws Exception {
-        if (!bound) {
-          semaphore.acquire();
-          if (!bound) {
-            startBindServiceAndDo(new ServiceConnectedCallback() {
-              @Override public void call() {
-                doCall(callback, emitter);
-                semaphore.release();
-              }
-            });
-          } else {
-            doCall(callback, emitter);
-            semaphore.release();
-          }
-        } else {
-          doCall(callback, emitter);
-        }
-      }
-    }).subscribeOn(Schedulers.io());
-  }
-
-  private void doCall(GeneralObservableCallback callback, ObservableEmitter<Object> emitter) {
-    if (callback != null) {
-      try {
-        callback.call();
-      } catch (Exception e) {
-        emitter.onError(e);
-      }
-    }
-    emitter.onNext(object);
-    emitter.onComplete();
-  }
-
-  /**
-   * start and bind service.
-   *
-   * @param callback Called when service connected.
-   */
-  private void startBindServiceAndDo(final ServiceConnectedCallback callback) {
-    Intent intent = new Intent(context, DownloadService.class);
-    intent.putExtra(DownloadService.INTENT_KEY, maxDownloadNumber);
-    context.startService(intent);
-    context.bindService(intent, new ServiceConnection() {
-      @Override public void onServiceConnected(ComponentName name, IBinder binder) {
-        DownloadService.DownloadBinder downloadBinder = (DownloadService.DownloadBinder) binder;
-        downloadService = downloadBinder.getService();
-        context.unbindService(this);
-        bound = true;
-        callback.call();
-      }
-
-      @Override public void onServiceDisconnected(ComponentName name) {
-        //注意!!这个方法只会在系统杀掉Service时才会调用!!
-        bound = false;
-      }
-    }, Context.BIND_AUTO_CREATE);
-  }
-
-  private interface GeneralObservableCallback {
-    void call() throws Exception;
-  }
-
-  private interface ServiceConnectedCallback {
-    void call();
   }
 }
